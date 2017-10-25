99aa3aa23a198dc131f57c6efa75a2218b0c3b10
Custu2000 <cristi_custura@yahoo.com>
2017-10-25 15:39:08 +0300

    fist commit on my_branch branch

--- src/square.py
+++ src/square.py
@@ -5,3 +5,12 @@ def my_square (y):
     return (y * * 2)
 
 print(my_square(42))
+def my_square2 (y):
+    """ just a comment
+    and a new line
+    ""
+    return (y * * 2)
+
+print(my_square2(2))
+
+
